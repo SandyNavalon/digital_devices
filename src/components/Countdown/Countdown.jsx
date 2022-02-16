f40a5e57fe86acb91a88788a6995fc0681af8a23
Sandra <sandy.navalon.tovar@gmail.com>
2022-02-16 17:14:18 +0100

    CHANGED countdown component function

--- src/components/Countdown/Countdown.jsx
+++ src/components/Countdown/Countdown.jsx
@@ -1,34 +1,91 @@
 import './Countdown.scss'
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Countdown = () => {
-    const [time, setTime] = useState('');
-    useEffect(() => {
-        let countdownDate = new Date('Nov 7, 2022 00:00:00').getTime();
+    const [timeDays, setTimeDays] = useState('00');
+    const [timeHours, setTimeHours] = useState('00');
+    const [timeMinutes, setTimeMinutes] = useState('00');
+    const [timeSeconds, setTimeSeconds] = useState('00');
 
-        let x = setInterval(() => {
-            let now = new Date().getTime();
-            let distance = countdownDate - now;
+    let interval = useRef();
+
+    const startCountdown = () => {
+        const countdownDate = new Date ('Nov 7, 2022 00:00:00').getTime();
+
+        interval = setInterval(() => {
+            const now = new Date().getTime();
+            const distance = countdownDate - now;
+
             let days = Math.floor(distance / (1000*60*60*24));
             let hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
             let minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
             let seconds = Math.floor((distance % (1000*60)) / 1000);
 
-            setTime(days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's');
 
             if(distance < 0) {
-                clearInterval(x);
-                setTime('COUNTDOWN IS OVER!')
+                //stop
+                clearInterval(interval.current);
+            }else {
+                //update
+                setTimeDays(days);
+                setTimeHours(hours);
+                setTimeMinutes(minutes);
+                setTimeSeconds(seconds);
             }
-        }, 1000);
-    }, []);
+
+        }, 1000)
+    };
+    useEffect(() => {
+        startCountdown();
+        return () => {
+            clearInterval(interval.current);
+        }
+    })
+
+    // useEffect(() => {
+    //     let countdownDate = new Date('Nov 7, 2022 00:00:00').getTime();
+
+    //     let x = setInterval(() => {
+    //         let now = new Date().getTime();
+    //         let distance = countdownDate - now;
+    //         let days = Math.floor(distance / (1000*60*60*24));
+    //         let hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+    //         let minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+    //         let seconds = Math.floor((distance % (1000*60)) / 1000);
+
+    //         setTime(days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's');
+
+    //         if(distance < 0) {
+    //             clearInterval(x);
+    //             setTime('COUNTDOWN IS OVER!')
+    //         }
+    //     }, 1000);
+    // }, []);
 
 
     return (
         <div className="countdown">
             <h2 className="countdown__title">Days until my bday</h2>
-            <h2 className="countdown__time">{time}</h2>
+            <div className="countdown__time">
+                <div className='countdown__time-unit'>
+                    <p>{timeDays}</p>
+                    <p className='countdown__time-unit-name'>Days</p>
+                </div>
+                <div className='countdown__time-unit'>
+                    <p>{timeHours}</p>
+                    <p className='countdown__time-unit-name'>Hours</p>
+                </div>
+
+                <div className='countdown__time-unit'>
+                    <p>{timeMinutes}</p>
+                    <p className='countdown__time-unit-name'>Minutes</p>
+                </div>
+                <div className='countdown__time-unit'>
+                    <p>{timeSeconds}</p>
+                    <p className='countdown__time-unit-name'>Seconds</p>
+                </div>
+            </div>
         </div>
     );
 }
